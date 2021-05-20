df085915cf07b37a80a85175f85e1515d8d66eeb
jonasreholt <jsf130@alumni.ku.dk>
2021-05-20 18:01:00 +0200

    added comments to prob 4

--- 4_largest_palindrome/largestPalindrome.hs
+++ 4_largest_palindrome/largestPalindrome.hs
@@ -3,6 +3,20 @@
 -- 2-digit numbers should give 9009 = 91 * 99
 -- 3-digit should be 906609.
 
+{-
+We brute force this problem. So for all number pairs between 100 and 999
+we check if it is a palindrome. If it is we add it to a result list; at the end
+we find the max value in that list.
+
+Note the following:
+    * let the number pair be n1 and n2, then we only check numbers where n1 <= n2
+      to avoid redundant checks.
+    * We cannot return the first palindrome we find, as we keep n2 high, and lower
+      n1 until it hits lower bound. Meaning we will not find 906609, for the 3-digit
+      test, as it will find a palindrom while n2=999 and n1 is descending. Hence
+      we list all found palindromes, and return max.
+-}
+
 reverseInt :: Int -> Int
 reverseInt n =
     revInt 0 n
